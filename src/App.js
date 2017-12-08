74cbc76bebb3c6be7d6bb73759da50cd2f88741e
Sam Scheffler <schefflersam@gmail.com>
2017-12-08 15:41:10 -0600

    Change var to const

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
-    var user = {
+    const helloWorld = 'Welcome to the Road to learn React';
+    const user = {
       firstName: 'Sam',
       lastName: 'Scheffler'
     }
