d22d91b0843087b37a261fc3bec8fd6f2941c388
Sam Scheffler <schefflersam@gmail.com>
2017-12-13 15:42:30 -0600

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,55 +1,69 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 const list = [
   {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1
   }
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list
+      list,
+      searchTerm: ""
     };
 
-    this.onDismiss = this
-      .onDismiss
-      .bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
 
-    const updatedList = this
-      .state
-      .list
-      .filter(isNotId);
+    const updatedList = this.state.list.filter(isNotId);
 
-    this.setState({list: updatedList})
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   render() {
+    const { searchTerm, list } = this.state;
+
     return (
       <div className="App">
-        {this
-          .state
-          .list
-          .map(item => <div key={item.objectID}>
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+
+        {list.filter(isSearched(searchTerm)).map(item => (
+          <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
@@ -57,11 +71,15 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button onClick={() => this.onDismiss(item.objectID)} type="button">
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
                 Dismiss
               </button>
             </span>
-          </div>)}
+          </div>
+        ))}
       </div>
     );
   }
