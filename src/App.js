6b1f3af4b6c210883dd7a823122a6940c68834ab
Sam Scheffler <schefflersam@gmail.com>
2017-12-22 18:20:12 -0800

    Add basic error handling

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
     this.state = {
       results: null,
       searchKey: "",
-      searchTerm: DEFAULT_QUERY
+      searchTerm: DEFAULT_QUERY,
+      error: null
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -53,7 +54,7 @@ class App extends Component {
     )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(e => e);
+      .catch(e => this.setState({ error: e }));
   }
 
   componentDidMount() {
@@ -94,7 +95,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
@@ -111,7 +112,13 @@ class App extends Component {
             Search
           </Search>
 
-          <Table list={list} onDismiss={this.onDismiss} />
+          {error ? (
+            <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>
+          ) : (
+            <Table list={list} onDismiss={this.onDismiss} />
+          )}
 
           <div className="interactions">
             <Button
